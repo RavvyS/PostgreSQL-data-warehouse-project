312d2b38a8679c7c9a1bc1ec865041d8b741440f
Ravindu S Hemachandra <156438913+RavvyS@users.noreply.github.com>
2025-04-14 23:44:42 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -12,86 +12,71 @@ scripts purpose:
 
 
 
-CALL bronze.load_bronze();
+-- Drop and create bronze.crm_cust_info
+DROP TABLE IF EXISTS bronze.crm_cust_info;
 
+CREATE TABLE bronze.crm_cust_info (
+    cst_id INT,
+    cst_key VARCHAR(50),
+    cst_firstname VARCHAR(50),
+    cst_lastname VARCHAR(50),
+    cst_marital_status VARCHAR(50),
+    cst_gndr VARCHAR(50),
+    cst_create_date DATE
+);
 
-CREATE OR REPLACE PROCEDURE bronze.load_bronze()
-LANGUAGE plpgsql
-AS $$
-DECLARE
-    start_time TIMESTAMP;
-    end_time TIMESTAMP;
-    batch_start_time TIMESTAMP := clock_timestamp();
-    batch_end_time TIMESTAMP;
-BEGIN
-    RAISE NOTICE 'LOADING Bronze layer';
-    RAISE NOTICE 'Loading CRM tables';
+-- Drop and create bronze.crm_prd_info
+DROP TABLE IF EXISTS bronze.crm_prd_info;
 
-    -- CRM Table 1
-    start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.crm_cust_info;
-    COPY bronze.crm_cust_info
-    FROM '/Users/ravindusandun/Documents/Ds/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
-    WITH (FORMAT csv, HEADER, DELIMITER ',');
-    end_time := clock_timestamp();
-    RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+CREATE TABLE bronze.crm_prd_info (
+    prd_id INT,
+    prd_key VARCHAR(50),
+    prd_nm VARCHAR(100),
+    prd_cost INT,
+    prd_line VARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE
+);
 
-    -- CRM Table 2
-    start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.crm_prd_info;
-    COPY bronze.crm_prd_info
-    FROM '/Users/ravindusandun/Documents/Ds/sql-data-warehouse-project/datasets/source_crm/prd_info.csv'
-    WITH (FORMAT csv, HEADER, DELIMITER ',');
-    end_time := clock_timestamp();
-    RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+-- Drop and create bronze.crm_sales_details
+DROP TABLE IF EXISTS bronze.crm_sales_details;
 
-    -- CRM Table 3
-    start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.crm_sales_details;
-    COPY bronze.crm_sales_details
-    FROM '/Users/ravindusandun/Documents/Ds/sql-data-warehouse-project/datasets/source_crm/sales_details.csv'
-    WITH (FORMAT csv, HEADER, DELIMITER ',');
-    end_time := clock_timestamp();
-    RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num VARCHAR(50),
+    sls_prd_key VARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT
+);
 
-    RAISE NOTICE 'Loading ERP tables';
+-- Drop and create bronze.erp_loc_a101
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
 
-    -- ERP Table 1
-    start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.erp_cust_az12;
-    COPY bronze.erp_cust_az12
-    FROM '/Users/ravindusandun/Documents/Ds/sql-data-warehouse-project/datasets/source_erp/CUST_AZ12.csv'
-    WITH (FORMAT csv, HEADER, DELIMITER ',');
-    end_time := clock_timestamp();
-    RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+CREATE TABLE bronze.erp_loc_a101 (
+    cid VARCHAR(50),
+    cntry VARCHAR(50)
+);
 
-    -- ERP Table 2
-    start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.erp_loc_a101;
-    COPY bronze.erp_loc_a101
-    FROM '/Users/ravindusandun/Documents/Ds/sql-data-warehouse-project/datasets/source_erp/LOC_A101.csv'
-    WITH (FORMAT csv, HEADER, DELIMITER ',');
-    end_time := clock_timestamp();
-    RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+-- Drop and create bronze.erp_cust_az12
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
 
-    -- ERP Table 3
-    start_time := clock_timestamp();
-    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-    COPY bronze.erp_px_cat_g1v2
-    FROM '/Users/ravindusandun/Documents/Ds/sql-data-warehouse-project/datasets/source_erp/PX_CAT_G1V2.csv'
-    WITH (FORMAT csv, HEADER, DELIMITER ',');
-    end_time := clock_timestamp();
-    RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM end_time - start_time);
+CREATE TABLE bronze.erp_cust_az12 (
+    cid VARCHAR(50),
+    bdate DATE,
+    gen VARCHAR(50)
+);
 
-    -- Final Summary
-    batch_end_time := clock_timestamp();
-    RAISE NOTICE 'Bronze layer completed successfully.';
-    RAISE NOTICE 'Total load duration: % seconds', EXTRACT(EPOCH FROM batch_end_time - batch_start_time);
+-- Drop and create bronze.erp_px_cat_g1v2
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
 
-EXCEPTION
-    WHEN OTHERS THEN
-        RAISE WARNING 'Something went wrong: %', SQLERRM;
-        RAISE WARNING 'ERROR occurred during loading Bronze layer.';
-END;
-$$;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id VARCHAR(50),
+    cat VARCHAR(50),
+    subcat VARCHAR(50),
+    maintenance VARCHAR(50)
+);
 
