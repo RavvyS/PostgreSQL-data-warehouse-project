cd55d984a2d69ef797d8a6a8b3fe6a4e21210d7e
Ravindu S Hemachandra <156438913+RavvyS@users.noreply.github.com>
2025-04-14 09:53:51 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,24 @@
+/*
+Create database and schemas-
 
+script purpose-
+frist we check weather the our database name is alredy exists or not , if exists we will drop that database and creta our database.
+additionaly we create a three schemas 'bronze','silver' and 'gold'.
+
+warning-
+BE CAUTON!!
+runnig this script will delete permanetely the existing database. so make sure that you already got a backup of the database..
+
+*/
+
+IF EXISTS (SELECT 1 FROM sys.database WHERE name='datawarehouse')
+BEGIN
+	ALTER DATABASE datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE datawarehouse;
+END;
+
+
+CREATE DATABASE datawarehouse;
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
